04c5238d1ef504fa39568183fad646a61dc80607
Joshua Pollak <abottomlesspit@gmail.com>
2021-05-24 19:10:31 -0700

    lint

--- src/Elephant.test.tsx
+++ src/Elephant.test.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import Elephant from './Elephant';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import Elephant from "./Elephant";
 
-test('renders learn react link', () => {
+test("renders learn react link", () => {
   render(<Elephant />);
-  const linkElement = screen.getByText(/test/i);
+  const linkElement = screen.getByText(/Elephant/i);
   expect(linkElement).toBeInTheDocument();
 });
