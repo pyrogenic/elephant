bfc217e77d33ce848a1c0270036205ed558475fb
Joshua Pollak <abottomlesspit@gmail.com>
2021-08-22 18:48:34 -0700

    linebreak formatting

--- src/DiscoTag.tsx
+++ src/DiscoTag.tsx
@@ -1,6 +1,7 @@
 import classConcat, { ClassNames } from "@pyrogenic/perl/lib/classConcat";
 import React from "react";
 import * as Router from "react-router-dom";
+import autoFormat from "./autoFormat";
 import DiscogsLinkback from "./DiscogsLinkback";
 import ElephantContext from "./ElephantContext";
 import { wrap } from "./LabelRoute";
@@ -15,13 +16,13 @@ export default function DiscoTag({ className, onClick, prewrap, src, uri: discog
     let li = false;
     let bold = 0;
     let italic = 0;
-  let underline = 0;
+    let underline = 0;
     let url: string[] = [];
     src.split(/-{4,}/).forEach((sect) => {
         if (result.length) {
             result.push(<hr key={result.length} />);
         }
-        sect.split(/(?<=[.!?])([\r\n]\n?)/).forEach((para) => {
+        sect.split(/(?<=[.!:\]?])\s*[\r\n]\n/).forEach((para) => {
             let matches: {
                 li?: string,
                 pre?: string,
@@ -33,16 +34,17 @@ export default function DiscoTag({ className, onClick, prewrap, src, uri: discog
             }
             const paragraph: JSX.Element[] = [];
             for (var groups of matches) {
+                const elements: JSX.Element[] = [];
                 if (groups.li) {
                     li = true;
                 }
                 const pre = groups.pre;
                 if (pre) {
                     const brs = pre.split(/[\r\n]\n?/);
-                    paragraph.push(<span key={paragraph.length}>{wrap(brs.pop(), bold, italic, underline, url)}</span>);
+                    elements.push(<span key={elements.length}>{wrap(brs.pop(), bold, italic, underline, url)}</span>);
                     while (brs.length) {
-                        paragraph.push(<br key={paragraph.length} />);
-                        paragraph.push(<span key={paragraph.length}>{wrap(brs.pop(), bold, italic, underline, url)}</span>);
+                        elements.push(<br key={elements.length} />);
+                        elements.push(<span key={elements.length}>{wrap(brs.pop(), bold, italic, underline, url)}</span>);
                     }
                 }
                 const fullTag = groups.tag;
@@ -51,47 +53,47 @@ export default function DiscoTag({ className, onClick, prewrap, src, uri: discog
                 if (tag === "l") {
                     const label = numericId ? lpdb.label(Number(tagId)) : lpdb.labels.values().find((l) => l.status === "ready" && l.value.name === tagId) ?? { status: "error" };
                     if (label.status !== "error") {
-                        paragraph.push(<Router.NavLink key={paragraph.length} exact to={`/labels/${tagId}`}><LoadingIcon remote={[label, "name"]} placeholder={tagId} /></Router.NavLink>);
+                        elements.push(<Router.NavLink key={elements.length} exact to={`/labels/${tagId}`}><LoadingIcon remote={[label, "name"]} placeholder={autoFormat(tagId)} /></Router.NavLink>);
                     } else if (tagId) {
-                        paragraph.push(<ExternalLink key={paragraph.length} href={`https://www.discogs.com/label/${encodeURIComponent(tagId)}`}>{tagId}</ExternalLink>);
+                        elements.push(<ExternalLink key={elements.length} href={`https://www.discogs.com/label/${encodeURIComponent(tagId)}`}>{autoFormat(tagId)}</ExternalLink>);
                     } else {
-                        paragraph.push(<span key={paragraph.length} className="text-warning">{tag}=undefined</span>);
+                        elements.push(<span key={elements.length} className="text-warning">{tag}=undefined</span>);
                     }
                 } else if (tag === "a") {
                     const artist = numericId ? lpdb.artistStore.get(numericId) : lpdb.artistStore.all.find(({ name }) => name === tagId);
                     if (artist) {
-                        paragraph.push(<Router.NavLink key={paragraph.length} exact to={`/artists/${tagId}`}>{artist.name ?? <LoadingIcon placeholder={tagId} />}</Router.NavLink>);
+                        elements.push(<Router.NavLink key={elements.length} exact to={`/artists/${tagId}`}>{artist.name ?? <LoadingIcon placeholder={autoFormat(tagId)} />}</Router.NavLink>);
                     } else if (tagId) {
-                        paragraph.push(<ExternalLink key={paragraph.length} href={`https://www.discogs.com/artist/${encodeURIComponent(tagId)}`}>{tagId}</ExternalLink>);
+                        elements.push(<ExternalLink key={elements.length} href={`https://www.discogs.com/artist/${encodeURIComponent(tagId)}`}>{autoFormat(tagId)}</ExternalLink>);
                     } else {
-                        paragraph.push(<span key={paragraph.length} className="text-warning">{tag}=undefined</span>);
+                        elements.push(<span key={elements.length} className="text-warning">{tag}=undefined</span>);
                     }
                 } else if (tag === "r") {
                     const collectionItem = numericId && lpdb.collection.values().find(({ id }) => id === numericId);
                     if (collectionItem) {
-                        paragraph.push(<Router.NavLink key={paragraph.length} exact to={`#${collectionItem.instance_id}`}>{collectionItem.basic_information.title}</Router.NavLink>);
+                        elements.push(<Router.NavLink key={elements.length} exact to={`#${collectionItem.instance_id}`}>{collectionItem.basic_information.title}</Router.NavLink>);
                     } else {
                         const release = numericId && lpdb.details({ id: numericId });
                         if (release) {
-                            paragraph.push(<ExternalLink key={paragraph.length} href={release.status === "ready" ? release.value.uri : undefined}><LoadingIcon remote={[release, "title"]} placeholder={tagId} /></ExternalLink>);
+                            elements.push(<ExternalLink key={elements.length} href={release.status === "ready" ? release.value.uri : undefined}><LoadingIcon remote={[release, "title"]} placeholder={autoFormat(tagId)} /></ExternalLink>);
                         } else {
-                            paragraph.push(<span key={paragraph.length} className="text-warning">{tagId}</span>);
+                            elements.push(<span key={elements.length} className="text-warning">{autoFormat(tagId)}</span>);
                         }
                     }
                 } else if (tag === "m") {
                     const collectionItem = numericId && lpdb.collection.values().find(({ basic_information: { master_id } }) => master_id === numericId);
                     if (collectionItem) {
-                        paragraph.push(<Router.NavLink key={paragraph.length} exact to={`#${collectionItem.instance_id}`}>{collectionItem.basic_information.title}</Router.NavLink>);
+                        elements.push(<Router.NavLink key={elements.length} exact to={`#${collectionItem.instance_id}`}>{collectionItem.basic_information.title}</Router.NavLink>);
                     } else {
                         const master = numericId && lpdb.master(numericId);
                         if (master && master.status === "ready" && master.value === "no-master-release") {
-                            paragraph.push(<span key={paragraph.length} className="text-warning">{tagId}</span>);
+                            elements.push(<span key={elements.length} className="text-warning">{autoFormat(tagId)}</span>);
                         } else if (master && master.status === "ready" && master.value !== "no-master-release") {
-                            paragraph.push(<ExternalLink key={paragraph.length} href={master.value.uri}></ExternalLink>);
+                            elements.push(<ExternalLink key={elements.length} href={master.value.uri}></ExternalLink>);
                         } else if (master && master.status !== "ready") {
-                            paragraph.push(<ExternalLink key={paragraph.length} href={undefined}><LoadingIcon placeholder={tagId} /></ExternalLink>);
+                            elements.push(<ExternalLink key={elements.length} href={undefined}><LoadingIcon placeholder={autoFormat(tagId)} /></ExternalLink>);
                         } else {
-                            paragraph.push(<span key={paragraph.length} className="text-warning">{tagId}</span>);
+                            elements.push(<span key={elements.length} className="text-warning">{autoFormat(tagId)}</span>);
                         }
                     }
                 } else if (tag === "b") {
@@ -99,9 +101,9 @@ export default function DiscoTag({ className, onClick, prewrap, src, uri: discog
                 } else if (tag === "/b") {
                     bold -= 1;
                 } else if (tag === "u") {
-                  underline += 1;
+                    underline += 1;
                 } else if (tag === "/u") {
-                  underline -= 1;
+                    underline -= 1;
                 } else if (tag === "i") {
                     italic += 1;
                 } else if (tag === "/i") {
@@ -111,10 +113,11 @@ export default function DiscoTag({ className, onClick, prewrap, src, uri: discog
                 } else if (tag === "/url") {
                     url.pop();
                 } else if (fullTag) {
-                    paragraph.push(<span key={paragraph.length} title={JSON.stringify({ fullTag, tag, tagId }, null, 2)}>[{fullTag}]</span>);
+                    elements.push(<span key={elements.length} title={JSON.stringify({ fullTag, tag, tagId }, null, 2)}>[{fullTag}]</span>);
                 }
+                paragraph.push(<React.Fragment key={paragraph.length}>{elements}</React.Fragment>)
             }
-          let content: Content = wrap(paragraph, bold, italic, underline, url);
+            let content: Content = wrap(paragraph, bold, italic, underline, url);
             if (li) {
                 li = false;
                 result.push(<li key={result.length}>{content}</li>);
