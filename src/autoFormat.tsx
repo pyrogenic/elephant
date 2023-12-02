6836d8577e77c785b58a1275f379df10726e4456
Joshua Pollak <abottomlesspit@gmail.com>
2023-12-02 15:38:51 -0800

    comment formatting

--- src/autoFormat.tsx
+++ src/autoFormat.tsx
@@ -42,13 +42,17 @@ export default function autoFormat(str: string | undefined) {
         default:
             // collapse all the ways weights are written
             str = str.replace(/(\d+)\s*gr?a?m?$/i, "$1g");
+
             // remove trailing numeric disambiguators from artist names
             str = str.replace(/ \(\d+\)$/, "");
+
             // smarten-up quotes
             str = str.replace(/(in|s)'(\s|$)/, "$1’$2");
             str = str.replace(/'s(\s|$)/, "’s$1");
             str = str.replace(/n't(\s|$)/, "n’t$1");
             str = str.replace(/ 'n(\s|$)/i, " ’n$1");
+
+            // apply traditional title-casing exceptions
             str = str.replace(/ Of /, " of ");
             str = str.replace(/ The /, " the ");
             str = str.replace(/ A /, " a ");
